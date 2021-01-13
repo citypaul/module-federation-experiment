837ac402c2842d1e74c06912562cd624d30c1ee0
Paul Hammond <paulhammond@hop-mac-hammop.broadband>
2021-01-13 12:34:41 +0000

    Initial products scaffolding

--- products/src/index.js
+++ products/src/index.js
@@ -2,9 +2,9 @@ import faker from "faker";
 
 let products = "";
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
   const name = faker.commerce.productName();
   products += `<div>${name}</div>`;
 }
 
-console.log(products);
+document.querySelector("#dev-products").innerHTML = products;
