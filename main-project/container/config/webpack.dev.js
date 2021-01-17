6994e6c2832f694ceba4cfa2c94051779f1bd05b
Paul Hammond <paul.hammond@gmail.com>
2021-01-17 13:59:00 +0000

    delegate shared deps

--- main-project/container/config/webpack.dev.js
+++ main-project/container/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
 
 const commonConfig = require("./webpack.common");
 
@@ -19,6 +20,7 @@ const devConfig = {
       remotes: {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
+      shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
