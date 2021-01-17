4396eda7c7f3a848e3de35ed53acf3102dd4405c
Paul Hammond <paul.hammond@gmail.com>
2021-01-17 13:04:22 +0000

    Container has some content so we can distinguish in browser

--- main-project/container/src/App.js
+++ main-project/container/src/App.js
@@ -2,5 +2,11 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <div>
+      <h1>Hello from container!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
