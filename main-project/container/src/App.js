d1f6725cf68a765b46961fe76231893e17edd27c
Paul Hammond <paul.hammond@gmail.com>
2021-01-17 13:03:16 +0000

    Import marketing app with custom react component inside container

--- main-project/container/src/App.js
+++ main-project/container/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
-
-console.log(mount);
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  return <h1>Hello world</h1>;
+  return <MarketingApp />;
 };
---
+++ main-project/container/src/components/MarketingApp.js
@@ -0,0 +1,12 @@
+import React, { useRef, useEffect } from "react";
+import { mount } from "marketing/MarketingApp";
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+};
