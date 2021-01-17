d2891aa939de2746206d796ee6a8b5257350cdd5
Paul Hammond <paul.hammond@gmail.com>
2021-01-17 12:11:29 +0000

    Initial webpack config to setup marketing app as a remote and container as host

--- main-project/container/src/App.js
+++ main-project/container/src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { mount } from "marketing/MarketingApp";
+
+console.log(mount);
 
 export default () => {
   return <h1>Hello world</h1>;
--- main-project/marketing/config/webpack.dev.js
+++ main-project/marketing/config/webpack.dev.js
@@ -1,5 +1,7 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
 const commonConfig = require("./webpack.common");
 
 const devConfig = {
@@ -11,6 +13,13 @@ const devConfig = {
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
