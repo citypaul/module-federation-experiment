ce341d276428ad2659a436af453dca51c2557814
Paul Hammond <paul.hammond@gmail.com>
2021-01-17 16:13:21 +0000

    prod build for container

--- main-project/container/config/webpack.dev.js
+++ main-project/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
 
@@ -21,9 +20,6 @@ const devConfig = {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
