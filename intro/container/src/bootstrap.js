0bd929cea208e3672267b967c3ed92548ee47f2a
Paul Hammond <paul.hammond@gmail.com>
2021-01-24 18:12:51 +0000

    remove main project folder

--- intro/container/src/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import { mount as productsMount } from "products/ProductsIndex";
-import { mount as cartMount } from "cart/CartShow";
-
-productsMount(document.querySelector("#container-products"));
-cartMount(document.querySelector("#container-cart"));
