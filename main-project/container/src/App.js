00978579a6d0700b7e60aac75cfdce2e321b0d4a
Paul Hammond <paul.hammond@gmail.com>
2021-01-24 11:34:52 +0000

    Change a value inside container to produce a different hash and see cache invalidation working on CI

--- main-project/container/src/App.js
+++ main-project/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hello from container!</h1>
+      <h1>Hello from container!!</h1>
       <hr />
       <MarketingApp />
     </div>
