3e311f05feab724373f9b1bf82554a1139f846e0
Paul Hammond <paul.hammond@gmail.com>
2021-01-24 17:22:04 +0000

    Child apps will use browser history when running in isolation in development mode

--- main-project/marketing/src/bootstrap.js
+++ main-project/marketing/src/bootstrap.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el, { onNavigate } = {}) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory } = {}) => {
+  const history = defaultHistory || createMemoryHistory();
 
   if (onNavigate) {
     history.listen(onNavigate);
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
