60ed74a78c66ff2bff1b80d6a85472e8242fc400
Paul Hammond <paul.hammond@gmail.com>
2021-01-31 14:18:30 +0000

    Vue dashboard works in isolation

--- dashboard/src/bootstrap.js
+++ dashboard/src/bootstrap.js
@@ -1,40 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createMemoryHistory, createBrowserHistory } from "history";
-import App from "./App";
+import { createApp } from "vue";
+import Dashboard from "./components/Dashboard.vue";
 
-const mount = (
-  el,
-  { onNavigate, defaultHistory, initialPath, onSignIn } = {}
-) => {
-  const history =
-    defaultHistory ||
-    createMemoryHistory({
-      initialEntries: [initialPath],
-    });
+const mount = (el) => {
+  const app = createApp(Dashboard);
 
-  if (onNavigate) {
-    history.listen(onNavigate);
-  }
-
-  ReactDOM.render(<App history={history} onSignIn={onSignIn} />, el);
-
-  return {
-    onParentNavigate({ pathname: nextPathName }) {
-      const { pathname } = history.location;
-
-      if (pathname !== nextPathName) {
-        history.push(nextPathName);
-      }
-    },
-  };
+  app.mount(el);
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_dashboard-dev-root");
 
   if (devRoot) {
-    mount(devRoot, { defaultHistory: createBrowserHistory() });
+    mount(devRoot);
   }
 }
 
