5674bc4a21e5ce4fb0752ac93fc3229eb10febac
Paul Hammond <paul.hammond@gmail.com>
2021-01-31 14:35:35 +0000

    vue dashboard (#7)
    * Initial configuration for vue dashboard
    
    * Vue dashboard works in isolation
    
    * bring dashboard into container and add to build

--- dashboard/config/webpack.dev.js
+++ dashboard/config/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
 
@@ -7,24 +8,28 @@ const commonConfig = require("./webpack.common");
 const devConfig = {
   mode: "development",
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath: "http://localhost:8083/",
   },
   devServer: {
-    port: 8080,
+    port: 8083,
     historyApiFallback: {
       index: "/index.html",
+    },
+    headers: {
+      "Access-control-Allow-Origin": "*",
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
+      name: "dashboard",
       filename: "remoteEntry.js",
-      remotes: {
-        marketing: "marketing@http://localhost:8081/remoteEntry.js",
-        auth: "auth@http://localhost:8082/remoteEntry.js",
-        dashboard: "dashboard@http://localhost:8083/remoteEntry.js",
+      exposes: {
+        "./DashboardApp": "./src/bootstrap",
       },
       shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
     }),
   ],
 };
