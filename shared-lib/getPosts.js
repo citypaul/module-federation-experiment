1d6b6c44b976a23e09ab1a62af9fbf4c60f39abd
Paul Hammond <paul.hammond@gmail.com>
2021-02-16 12:36:48 +0000

    Api layer (#9)
    * add libs
    
    * working example
    
    * better implementation
    
    * prevent extra api call
    
    * use lib so it works in safari

---
+++ shared-lib/getPosts.js
@@ -0,0 +1,47 @@
+import { BroadcastChannel } from "broadcast-channel";
+const bc = new BroadcastChannel("test_channel");
+
+const keys = {
+  allPosts: "allPosts",
+};
+
+const createBroadcastChannelData = ({ key }) => ({ key });
+
+export const getPosts = (onUpdateCallback) => {
+  bc.onmessage = (data) => {
+    if (data.key === keys.allPosts) {
+      onUpdateCallback();
+    }
+  };
+
+  const fetchPosts = () => {
+    console.log("fetching!");
+    return fetch("http://localhost:3000/posts").then((response) =>
+      response.json()
+    );
+  };
+
+  return {
+    fetchPosts,
+  };
+};
+
+export const createPost = ({ author, title }) => {
+  // Default options are marked with *
+  return fetch("http://localhost:3000/posts", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({ title, author }), // body data type must match "Content-Type" header
+  }).then((res) => {
+    bc.postMessage(createBroadcastChannelData({ key: keys.allPosts }));
+    return res;
+  });
+};
