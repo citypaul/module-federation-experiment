f3427e108b397fd7f147c87ab124604f0ccd41c8
Paul Hammond <paul.hammond@gmail.com>
2021-02-17 12:58:09 +0000

    working state

--- redux-app/src/sagas.bak.js
+++ redux-app/src/sagas.bak.js
@@ -5,21 +5,36 @@ import { getPosts, createPost } from "../../shared-lib/getPosts";
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-function* helloSaga() {
-  const postsChannel = eventChannel((emitter) => {
-    const { fetchPosts } = getPosts({
+// factory to return a channel that emits based on broadcast messages
+const createChannel = (fetcher, cacheStrategy, ...args) =>
+  eventChannel((emitter) => {
+    const { fetch } = fetcher(cacheStrategy, {
       onGlobalUpdate: () => {
-        fetchPosts().then((data) => emitter(data));
+        fetch(...args).then((data) => emitter(data));
       },
     });
-    fetchPosts().then((data) => emitter(data));
+    fetch(...args).then((data) => emitter(data));
     return () => {
       // cleanup
     };
   });
+
+function* handleMyActionDispatched(action) {
+  // get data from action
+  const { payload } = action;
+  // fetch data from store
+  const data = yield select(selectData);
+  // fetch and subscribe to posts data passing in args
+  const postsChannel = createChannel(getPosts, data, payload);
+  // not an infinite loop
   while (true) {
+    // everytime the channel emits we take
     const data = yield take(postsChannel);
-    console.log("emitted", data);
+    // submit the data to store
+    yield put({
+      type: "posts fetched (saga)",
+      payload: data,
+    });
   }
 }
 
@@ -40,4 +55,6 @@ function* watchIncrementAsync() {
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
   yield all([helloSaga(), watchIncrementAsync()]);
+
+  yield takeLatest("my action", handleMyActionDispatched);
 }
